cf5eae2a66e5fbc5462ac052d1b2952fefd535a1
Ananya Garg <akgarg@unc.edu>
2022-10-22 21:18:08 -0400

    file structure

---
+++ Connect2Vote/ViewRouter.swift
@@ -0,0 +1,23 @@
+//
+//  ViewRouter.swift
+//  Connect2Vote
+//
+//  Created by Ananya Garg on 10/22/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+
+
+class ViewRouter: ObservableObject{
+    
+    
+    
+}
+
+
+enum Page{
+    
+}
---
+++ Connect2Vote/Views/ParentView.swift
@@ -0,0 +1,37 @@
+//
+//  ParentView.swift
+//  Connect2Vote
+//
+//  Created by Ananya Garg on 10/22/22.
+//
+
+import SwiftUI
+
+struct ParentView: View {
+    
+    @EnvironmentObject var viewRouter: ViewRouter
+    
+    var body: some View {
+        switch viewRouter.currentPage{
+        case .signUpPage:
+            SignUpView()
+        case .signInPage:
+            SignInView()
+//         case .mapPage:
+//            MapView()
+        case .addFriendsPage:
+            AddFriendsView()
+        case .requestPage:
+            RequestView()
+        case .myFriendPage:
+            MyFriendsView()
+        }
+       
+    }
+}
+
+struct ParentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParentView().environmentObject(ViewRouter())
+    }
+}
